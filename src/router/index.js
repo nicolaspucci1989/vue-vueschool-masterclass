eb09046f1e85e1c47253476e752450e61fbf9b7e
Nicolas Pucci <nicolaspucci11989@gmail.com>
2022-06-09 20:10:36 -0300

    feat: not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/components/PageHome'
 import PageThreadShow from '@/components/PageThreadShow'
+import PageNotFound from '@/components/PageNotFound'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'ThreadShow',
     component: PageThreadShow,
     props: true
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: PageNotFound
   }
 ]
 
