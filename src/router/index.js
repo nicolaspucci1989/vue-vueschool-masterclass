7ea03fcba40925ef12968efdb7aed17133f9775a
Nicolas Pucci <nicolaspucci11989@gmail.com>
2022-06-10 19:46:48 -0300

    refactor: reorganize components

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import PageHome from '@/components/PageHome'
-import PageThreadShow from '@/components/PageThreadShow'
-import PageNotFound from '@/components/PageNotFound'
+import Home from '@/pages/Home'
+import ThreadShow from '@/pages/ThreadShow'
+import NotFound from '@/pages/NotFound'
 import sourceData from '@/data.json'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: PageHome
+    component: Home
   },
   {
     path: '/thread/:id',
     name: 'ThreadShow',
-    component: PageThreadShow,
+    component: ThreadShow,
     props: true,
     beforeEnter (to, from, next) {
       const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
@@ -32,7 +32,7 @@ const routes = [
   {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
-    component: PageNotFound
+    component: NotFound
   }
 ]
 
