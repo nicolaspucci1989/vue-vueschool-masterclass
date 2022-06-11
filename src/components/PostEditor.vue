b99b0747ec0dd4eae9600feebf7ac4acdbcc9064
Nicolas Pucci <nicolaspucci11989@gmail.com>
2022-06-10 21:58:29 -0300

    feat: post editor

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="col-full">
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <textarea v-model="text" name="" id="" cols="30" rows="10" class="form-input"/>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit Post</button>
+      </div>
+    </form>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'PostEditor',
+  data () {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    save () {
+      const postId = 'gggg' + Math.random()
+      const post = {
+        id: postId,
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        userId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1'
+      }
+      this.$emit('save', { post })
+      // this.posts.push(post)
+      // this.thread.posts.push(postId)
+
+      this.newPostText = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -2,27 +2,18 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
     <post-list :posts="threadPosts"/>
-
-    <div class="col-full">
-      <form @submit.prevent="addPost">
-        <div class="form-group">
-          <textarea v-model="newPostText" name="" id="" cols="30" rows="10" class="form-input"/>
-        </div>
-        <div class="form-actions">
-          <button class="btn-blue">Submit Post</button>
-        </div>
-      </form>
-    </div>
+    <post-editor @save="addPost"/>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data.json'
 import PostList from '@/components/PostList'
+import PostEditor from '@/components/PostEditor'
 
 export default {
   name: 'ThreadShow',
-  components: { PostList },
+  components: { PostEditor, PostList },
   props: {
     id: {
       type: String,
@@ -32,8 +23,7 @@ export default {
   data () {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts,
-      newPostText: ''
+      posts: sourceData.posts
     }
   },
   computed: {
@@ -45,19 +35,13 @@ export default {
     }
   },
   methods: {
-    addPost () {
-      const postId = 'gggg' + Math.random()
+    addPost (eventData) {
       const post = {
-        id: postId,
-        text: this.newPostText,
-        publishedAt: Math.floor(Date.now() / 1000),
-        threadId: this.id,
-        userId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1'
+        ...eventData.post,
+        threadId: this.id
       }
       this.posts.push(post)
-      this.thread.posts.push(postId)
-
-      this.newPostText = ''
+      this.thread.posts.push(post.id)
     }
   }
 }
