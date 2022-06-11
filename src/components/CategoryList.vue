7ecc7846b3d076c8bc91f6f965384ecfceed2219
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-06-11 18:14:19 -0300

    Module 03 (#1)
    * add styles, html
    
    * styles
    
    * add vue router
    
    * refactor: extract router
    
    * feat: thread component
    
    * feat: thread link
    
    * feat: not found page
    
    * feat: maintain url when redirect
    
    * refactor: reorganize components
    
    * feat: post list component
    
    * feat: add thread post
    
    * feat: post editor
    
    * feat: human friendly dates
    
    * feat: app date component
    
    * feat: global component registration
    
    * feat: forum page
    
    * feat: forum list
    
    * feat: category list
    
    * refactor: category list
    
    Co-authored-by: Nicolas Pucci <nicolaspucci11989@gmail.com>

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,34 @@
+<template>
+  <forum-list
+    v-for="category in categories"
+    :key="category.id"
+    :forums="getForumsFromCategory(category)"
+    :title="category.name"
+    :categoryId="category.id"
+  />
+</template>
+
+<script>
+import ForumList from '@/components/ForumList'
+import sourceData from '@/data.json'
+
+export default {
+  categoryName: 'CategoryList',
+  components: { ForumList },
+  props: {
+    categories: {
+      required: true,
+      type: Array
+    }
+  },
+  methods: {
+    getForumsFromCategory (category) {
+      return sourceData.forums.filter(forum => forum.categoryId === category.id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
