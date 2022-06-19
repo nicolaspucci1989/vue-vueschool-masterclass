dfcbce2c34c5395c3cf0616570218dd1d770a53e
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-06-19 16:26:10 -0300

    add spinner (lesson 78)

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <the-navbar/>
   <div class="container">
     <router-view v-show="showPage" @ready="showPage = true"/>
-    <div v-show="!showPage" class="push-top">loading...</div>
+    <app-spinner v-show="!showPage"/>
   </div>
 </template>
 
