e1765064f0169206d7814442e3a68732855f93fd
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-06-25 22:25:30 -0300

    import modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,22 @@
 import { createStore } from 'vuex'
 import actions from '@/store/actions'
 import mutations from '@/store/mutations'
-import threads from './modules/threads'
+
 import auth from './modules/auth'
-import forums from './modules/forums'
 import users from './modules/users'
+import forums from './modules/forums'
+import threads from './modules/threads'
+import categories from './modules/categories'
+import posts from './modules/posts'
 
 export default createStore({
   modules: {
     auth,
     users,
     forums,
-    threads
+    threads,
+    posts,
+    categories
   },
   state: {
     unsubscribes: []
