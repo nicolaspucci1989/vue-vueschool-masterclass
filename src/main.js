7e76c87ab2b92d135a5ca5477d48d98922f7d381
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-07-02 14:19:36 -0300

    Module 14 (#11)
    * add click outside directive (lesson 106)
    
    * mobile navbar (lesson 107)
    
    * close nav when scrolling (lesson 108)

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 import FontAwesome from '@/plugins/FontAwesome'
+import ClickOutside from '@/plugins/ClickOutsideDirective'
+import PageScroll from '@/plugins/PageScrollDirective'
 
 const forumApp = createApp(App)
 forumApp.use(router)
 forumApp.use(store)
 forumApp.use(FontAwesome)
+forumApp.use(ClickOutside)
+forumApp.use(PageScroll)
 
 const requireComponent = require.context('./components', true, /App[A-Z]\w+\.(vue|js)$/)
 requireComponent.keys().forEach(function (fileName) {
