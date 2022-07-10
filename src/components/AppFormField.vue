20ba9fd8a8276681e70b25509ae65a039dc39aab
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-07-10 17:53:32 -0300

    add form field component (lesson 134)

---
+++ src/components/AppFormField.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="form-group">
+    <label :for="name">{{label}}</label>
+    <VeeField
+      :id="name"
+      :label="label"
+      :modelValue="modelValue"
+      :name="name"
+      class="form-input"
+      v-bind="$attrs"
+      @input="$emit('modelUpdate', $event)"
+    />
+    <VeeErrorMessage :name="name" class="form-error" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: { type: String, required: true },
+    label: { type: String, required: true },
+    modelValue: { type: String, default: '' }
+  }
+}
+</script>
